f9471840e10c55f7f0077530082a8e06fb01bec8
Karen Costa <karen.pcosta@gmail.com>
2022-11-10 03:04:10 -0300

    Add changeDogProfile function

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 import dogs from "./dogDB.js";
 import DogTemplate from "./dogTemplate.js";
-// import getNewProfile from "./utils.js";
 
 const userContainer = document.getElementById("userContainer");
-// const likeBtn = document.getElementById("likeBtn");
-// const rejectBtn = document.getElementById("rejectBtn");
-let currentDog = 0;
-let introDog = new DogTemplate(dogs[currentDog]);
+const btnContainer = document.getElementById("buttonsContainer");
+const likeBtn = document.getElementById("likeBtn");
+
+likeBtn.addEventListener("click", render);
+
+let dogsNames = dogs.map(function (dog) {
+  return dog.name;
+});
+
+function changeDogProfile() {
+  let currentDog = dogsNames.shift();
+  const nextDogProfile = dogs.find((value) => value.name === currentDog);
+  return nextDogProfile ? new DogTemplate(nextDogProfile) : {};
+}
 
 function render() {
-  document.getElementById("userContainer").innerHTML = introDog.getDogProfile();
+  let dog = changeDogProfile();
+  document.getElementById("userContainer").innerHTML = dog.getDogProfile();
 }
 
 render();
