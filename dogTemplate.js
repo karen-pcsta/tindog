4c191051a16b00d8e4568d67215eab1c2f9367c2
Karen Costa <karen.pcosta@gmail.com>
2022-11-11 03:37:40 -0300

    Fix typo on import

--- dogTemplate.js
+++ dogTemplate.js
@@ -1,24 +1,28 @@
+import dogs from "./dogDB.js";
+
 class DogTemplate {
   constructor(data) {
     Object.assign(this, data);
   }
   getDogProfile() {
     const { name, avatar, age, bio } = this;
-    return `<div class="user-picture">
+    return `
+        <div class="user-picture">
         <img src="${avatar}" alt="image of a dog"
         </div>
         <div class="user-info">
           <h2 class="user-name">${name},</h2>
           <h2 class="user-age">${age}</h2>
           <p class="user-bio">${bio} </p>
-        </div>
-        <div class="like-badge">
-          <img src="./images/badge-like.png" alt="image with like written on it" />
-        </div>
-        <div class="nope-badge">
-          <img src="./images/badge-nope.png" alt="image with nope written on it" />
-        </div>
-      </div>`;
+       </div>`;
+  }
+  changeStatusToLike(status) {
+    this.hasBeenSwiped = status;
+    this.hasBeenLiked = true;
+  }
+  changeStatusToDislike(status) {
+    this.hasBeenSwiped = status;
+    this.hasBeenLiked = false;
   }
 }
 
